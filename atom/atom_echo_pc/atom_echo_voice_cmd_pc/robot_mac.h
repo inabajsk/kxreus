58e086faf5ac46d4e09f0af5e1630847c704d2f0
Masayuki Inaba <inaba@jsk.imi.i.u-tokyo.ac.jp>
2026-09-01 14:53:51 +0900

    debug

--- atom/atom_echo_pc/atom_echo_voice_cmd_pc/robot_mac.h
+++ atom/atom_echo_pc/atom_echo_voice_cmd_pc/robot_mac.h
@@ -1,5 +1,5 @@
-// 自動生成ファイル: ../../atom_echo_robot/get_my_mac.sh でロボット側ATOM Echo
-// (ポート /dev/ttyUSB1)の実MACアドレスを読み取って生成した。手動編集しないこと。
-// 生成元MAC: 64:b7:08:88:8a:cc
+// 自動生成ファイル: get_my_mac.sh でこのAtomS3(ポート /dev/ttyACM0)の実MAC
+// アドレスを読み取って生成した。手動編集しないこと。
+// 生成元MAC: dc:54:75:cb:d7:ac
 #pragma once
-#define ROBOT_MAC_BYTES { 0x64, 0xB7, 0x08, 0x88, 0x8A, 0xCC }
+#define ROBOT_MAC_BYTES { 0xDC, 0x54, 0x75, 0xCB, 0xD7, 0xAC }
